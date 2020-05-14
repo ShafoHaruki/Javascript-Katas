6bd0f9042b953120906f66a25a15c5fc944214a3
ShafoHaruki <muhammadsyafizuraimi@gmail.com>
2020-05-14 19:58:19 +0800

    added refactoring but it cannot work

--- 6kyu_countTheSmileyFaces.js
+++ 6kyu_countTheSmileyFaces.js
@@ -20,30 +20,6 @@ Note: In case of an empty array return 0. You will not be tested with invalid in
 Happy coding!
 */
 
-// function countSmileys(arr) {
-//   let count = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     const twoLengthFace = arr[i].length == 2;
-//     const threeLengthFace = arr[i].length == 3;
-//     const ele1 = arr[i][0];
-//     const ele2 = arr[i][1];
-//     const ele3 = arr[i][2];
-//     if (threeLengthFace) {
-//       if (
-//         (ele1 === ":" || ele1 === ";") &&
-//         (ele2 === "-" || ele2 === "~") &&
-//         (ele3 === ")" || ele3 === "D")
-//       )
-//         count++;
-//     }
-//     if (twoLengthFace) {
-//       if ((ele1 === ":" || ele1 === ";") && (ele2 === ")" || ele2 === "D"))
-//         count++;
-//     }
-//   }
-//   return count;
-// }
-
 function countSmileys(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -68,5 +44,22 @@ function countSmileys(arr) {
   }
   return count;
 }
+
+// //It cannot work after refactoring
+// function countSmileys(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     const noNoseFace = arr[i].length == 2;
+//     const withNoseFace = arr[i].length == 3;
+//     const eyePart = (arr[i][0] == ":" || arr[i][0] == ";");
+//     const nosePart = (arr[i][1] == "-" || arr[i][1] == "~");
+//     const mouthPart = (arr[i][2] == ")" || arr[i][2] == "D");
+//     if (noNoseFace && eyePart && mouthPart
+//     ) count++;
+//     if (withNoseFace && eyePart && nosePart && mouthPart
+//     ) count++;
+//   }
+//   return count;
+// }
 
 countSmileys([";D", ":-(", ":-)", ";~)"]); //outputs 3
